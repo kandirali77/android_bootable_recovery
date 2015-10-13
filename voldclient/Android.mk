62d84f6c206e0c3a82a96eaf464b008540e37a12
kandirali77 <kandirali77@gmail.com>
2015-10-13 03:04:55 +0300

    Update Android.mk
    changed cflags for giving no errors for unused-parameter, shadow and noreturn

--- voldclient/Android.mk
+++ voldclient/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libvoldclient
 LOCAL_SRC_FILES := commands.c dispatcher.c event_loop.c
-LOCAL_CFLAGS := -DMINIVOLD -Werror
+LOCAL_CFLAGS := -DMINIVOLD -Werror -Wno-error=unused-parameter -Wno-error=shadow -Wno-error=suggest-attribute=noreturn
 LOCAL_C_INCLUDES :=         	\
     $(LOCAL_PATH)/..       	\
     system/core/fs_mgr/include	\
